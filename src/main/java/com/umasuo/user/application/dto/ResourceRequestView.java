39d9b8033e4dc449c5e23f5df11a24b0ce7ecf72
davis999 <davis.dai@go6d.com>
2017-06-09 18:20:20 +0800

    edit view model

--- src/main/java/com/umasuo/user/application/dto/ResourceRequestView.java
+++ src/main/java/com/umasuo/user/application/dto/ResourceRequestView.java
@@ -1,8 +1,78 @@
 package com.umasuo.user.application.dto;
 
+import com.umasuo.user.infrastructure.enums.ReplyRequest;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
 /**
  * Created by Davis on 17/6/9.
  */
+@Setter
+@Getter
+@ToString
 public class ResourceRequestView {
-// TODO: 17/6/9
+  /**
+   * The request id.
+   */
+  private String id;
+
+  /**
+   * The Created at.
+   */
+  private ZonedDateTime createdAt;
+
+  /**
+   * The Last modified at.
+   */
+  private ZonedDateTime lastModifiedAt;
+
+  /**
+   * Version used for update date check.
+   */
+  private Integer version;
+
+  /**
+   * The Acceptor id.
+   */
+  private String acceptorId;
+
+  /**
+   * The Acceptor user id.
+   */
+  private String acceptorUserId;
+
+  /**
+   * The Reply request.
+   */
+  private ReplyRequest replyRequest;
+
+  /**
+   * The Applicant id.
+   */
+  private String applicantId;
+
+  /**
+   * The Applicant user id.
+   */
+  private String applicantUserId;
+
+  /**
+   * The Applicant viewed.
+   */
+  private Boolean applicantViewed;
+
+  /**
+   * The Device id.
+   */
+  private String deviceId;
+
+  /**
+   * The References.
+   */
+  private List<Reference> references;
 }
--- src/main/java/com/umasuo/user/application/dto/mapper/ResourceRequestMapper.java
+++ src/main/java/com/umasuo/user/application/dto/mapper/ResourceRequestMapper.java
@@ -1,5 +1,6 @@
 package com.umasuo.user.application.dto.mapper;
 
+import com.google.common.collect.Lists;
 import com.umasuo.user.application.dto.PermissionRequest;
 import com.umasuo.user.application.dto.ResourceRequestView;
 import com.umasuo.user.domain.model.DeveloperUser;
@@ -7,6 +8,7 @@ import com.umasuo.user.domain.model.ResourceRequest;
 import com.umasuo.user.infrastructure.enums.ReplyRequest;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * Created by Davis on 17/6/9.
@@ -19,6 +21,13 @@ public final class ResourceRequestMapper {
   private ResourceRequestMapper() {
   }
 
+  /**
+   * Build resource request.
+   *
+   * @param request the request
+   * @param user the user
+   * @return the resource request
+   */
   public static ResourceRequest build(PermissionRequest request, DeveloperUser user) {
     ResourceRequest result = new ResourceRequest();
 
@@ -32,13 +41,42 @@ public final class ResourceRequestMapper {
     return result;
   }
 
+  /**
+   * To model list.
+   *
+   * @param entities the entities
+   * @return the list
+   */
   public static List<ResourceRequestView> toModel(List<ResourceRequest> entities) {
-    // TODO: 17/6/9
-    return null;
+    List<ResourceRequestView> models = Lists.newArrayList();
+
+    Consumer<ResourceRequest> consumer = request -> models.add(toModel(request));
+
+    entities.stream().forEach(consumer);
+
+    return models;
   }
 
-  public static ResourceRequestView toModel(ResourceRequest updatedRequest) {
-    // TODO: 17/6/9
-    return null;
+  /**
+   * To model resource request view.
+   *
+   * @param entity the entity
+   * @return the resource request view
+   */
+  public static ResourceRequestView toModel(ResourceRequest entity) {
+    ResourceRequestView model = new ResourceRequestView();
+
+    model.setId(entity.getId());
+    model.setVersion(entity.getVersion());
+    model.setApplicantViewed(entity.getApplicantViewed());
+    model.setApplicantUserId(entity.getApplicantUserId());
+    model.setApplicantId(entity.getApplicantId());
+    model.setAcceptorId(entity.getAcceptorId());
+    model.setAcceptorUserId(entity.getAcceptorUserId());
+    model.setDeviceId(entity.getDeviceId());
+    model.setReplyRequest(entity.getReplyRequest());
+    model.setReferences(entity.getReferences());
+
+    return model;
   }
 }
