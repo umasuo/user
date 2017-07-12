c242785e6c66608e3fe5f5331d7ff32308deb94a
umasuo <liuquan89@gmail.com>
2017-07-12 18:58:47 +0800

    update

--- src/main/java/com/umasuo/user/infrastructure/config/AppConfig.java
+++ src/main/java/com/umasuo/user/infrastructure/config/AppConfig.java
@@ -26,24 +26,24 @@ public class AppConfig {
   /**
    * message broker's host.
    */
-  @Value("${message-broker.host:127.0.0.1}")
+  @Value("${message.broker.host:127.0.0.1}")
   public String msgBrokerHost;
 
   /**
    * message broker's port
    */
-  @Value("${message-broker.port:1883}")
+  @Value("${message.broker.port:1883}")
   public int msgBrokerPort;
 
   /**
    * super user's username for message broker.
    */
-  @Value("${message-broker.username:umasuo}")
+  @Value("${message.broker.username:umasuo}")
   public String username;
 
   /**
    * super user's password for message broker.
    */
-  @Value("${message-broker.password:password}")
+  @Value("${message.broker.password:password}")
   public String password;
 }
