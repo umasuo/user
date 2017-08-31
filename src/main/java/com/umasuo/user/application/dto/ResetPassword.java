b551a24c5650df0cc85dc6424df5bf9aad4cdcb5
umasuo <liuquan89@gmail.com>
2017-08-31 14:36:45 +0800

    update

--- src/main/java/com/umasuo/user/application/dto/ResetPassword.java
+++ src/main/java/com/umasuo/user/application/dto/ResetPassword.java
@@ -5,15 +5,33 @@ import lombok.Data;
 import java.io.Serializable;
 
 /**
- * Created by umasuo on 17/7/21.
+ * Updater for reset password.
  */
 @Data
 public class ResetPassword implements Serializable {
 
+  /**
+   * Auto generated serial version id.
+   */
   private static final long serialVersionUID = 2333151638836537082L;
 
+  /**
+   * Phone.
+   */
   private String phone;
+
+  /**
+   * Sms code.
+   */
   private String smsCode;
+
+  /**
+   * Password.
+   */
   private String password;
+
+  /**
+   * DeveloperId.
+   */
   private String developerId;
 }
